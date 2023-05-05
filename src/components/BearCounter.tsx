543fa448b0ae8a1545d71c732d2d2381652f5de8
Snow31lind <tienliquang@gmail.com>
2023-05-05 12:02:25 +0700

    Split state into data and action, implement bear form

--- src/components/BearCounter.tsx
+++ src/components/BearCounter.tsx
@@ -1,15 +1,86 @@
-import useStore from '../store';
+import { useState } from 'react';
+import useStore, { Bear } from '../store';
+
+interface IBear {
+  bear: Bear;
+}
+
+const defaultBearInformation = (): Bear => ({
+  name: '',
+  age: 0,
+  dateOfBirth: new Date(),
+  origin: 'Vietnam',
+});
+
+const Bear = ({ bear }: IBear) => {
+  const increaseAge = useStore((state) => state.increaseAge);
+  const decreaseAge = useStore((state) => state.decreaseAge);
+
+  return (
+    <div style={{ border: '1px solid coral' }}>
+      <p>{`Name: ${bear.name}`}</p>
+      <p>{`Age: ${bear.age}`}</p>
+      <p>{`Date of birth: ${bear.dateOfBirth.toLocaleString()}`}</p>
+      <p>{`Origin: ${bear.origin}`}</p>
+      <button onClick={() => increaseAge(bear.name)}>Increase Age</button>
+      <button onClick={() => decreaseAge(bear.name)}>Decrease Age</button>
+    </div>
+  );
+};
 
 const BearCounter = () => {
+  const [information, setInformation] = useState<Bear>(
+    defaultBearInformation()
+  );
+
   const bears = useStore((state) => state.bears);
-  const increase = useStore((state) => state.increasePopulation);
-  const release = useStore((state) => state.releasePopulation);
+  const insert = useStore((state) => state.insert);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    insert(information);
+    setInformation(defaultBearInformation());
+  };
 
   return (
     <div>
-      <p>{`Bear population: ${bears}`}</p>
-      <button onClick={increase}>+ Population</button>
-      <button onClick={release}>Relase</button>
+      <p>Bear information</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Bear's name"
+          value={information.name}
+          onChange={(event) =>
+            setInformation((prevInformation) => ({
+              ...prevInformation,
+              name: event.target.value,
+            }))
+          }
+        />
+        <input
+          placeholder="Bear's age"
+          value={information.age}
+          onChange={(event) =>
+            setInformation((prevInformation) => ({
+              ...prevInformation,
+              age: Number(event.target.value || 0),
+            }))
+          }
+        />
+        <input
+          placeholder="Bear's origin"
+          value={information.origin}
+          onChange={(event) =>
+            setInformation((prevInformation) => ({
+              ...prevInformation,
+              origin: event.target.value,
+            }))
+          }
+        />
+        <button type='submit'>Insert</button>
+      </form>
+      {bears.map((bear) => (
+        <Bear key={bear.name} bear={bear} />
+      ))}
     </div>
   );
 };
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,46 @@
 import { create } from 'zustand';
 
-const useStore = create((set) => ({
-  bears: 0,
-  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
-  releasePopulation: () => set((state) => ({ bears: 0 })),
+export interface Bear {
+  name: string;
+  age: number;
+  dateOfBirth: Date;
+  origin: string;
+}
+
+interface BearState {
+  bears: Bear[];
+}
+
+interface BearAction {
+  insert: (bear: Bear) => void;
+  increaseAge: (name: Bear['name']) => void;
+  decreaseAge: (name: Bear['name']) => void;
+}
+
+const useStore = create<BearState & BearAction>((set) => ({
+  bears: [
+    {
+      name: 'Koala',
+      age: 2,
+      dateOfBirth: new Date(2022, 8, 17),
+      origin: 'China',
+    },
+  ],
+  insert: (bear: Bear) => set((state) => ({ bears: [...state.bears, bear] })),
+  increaseAge: (name: Bear['name']) =>
+    set((state) => {
+      return {
+        bears: state.bears.map((bear) =>
+          bear.name !== name ? bear : { ...bear, age: bear.age + 1 }
+        ),
+      };
+    }),
+  decreaseAge: (name: Bear['name']) =>
+    set((state) => ({
+      bears: state.bears.map((bear) =>
+        bear.name !== name ? bear : { ...bear, age: bear.age - 1 }
+      ),
+    })),
 }));
 
 export default useStore;
